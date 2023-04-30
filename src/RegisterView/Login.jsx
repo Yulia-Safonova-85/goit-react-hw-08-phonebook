b2a637ec92e88cbf0857065958b2caa62cefb7eb
Yulia-Safonova-85 <109626421+Yulia-Safonova-85@users.noreply.github.com>
2023-04-30 21:21:52 +0300

    some update in Login form

--- src/RegisterView/Login.jsx
+++ src/RegisterView/Login.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 import authOperations from '../redux/Auth/authOperation';
-import {TextField, Button, Typography }from '@mui/material';
+import {TextField, Button, Typography, Box }from '@mui/material';
 
 export default function Login(){
     const dispatch = useDispatch();
@@ -32,24 +32,37 @@ const handleSubmit = evt => {
 variant="h6"
 sx={{mt:4}}
 >LogIn Page</Typography>
-
-<form onSubmit={handleSubmit}>
+<Box
+      component="form"
+      sx={{
+        '& > :not(style)': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off"
+      onSubmit={handleSubmit}
+    >
 
 <TextField 
  label="Email"
-  variant="outlined" 
+ id="outlined-controlled"
+ variant="outlined" 
   sx={{
     mr:'2rem'
   }}
-  type="email" name="email" onChange={handleChange}/>
+  type="email" name="email" value={email} onChange={handleChange}/>
   
-<TextField label="Password"
+<TextField 
+  label="Password"
+  id="password"
   variant="outlined" 
   sx={{
     mr:'2rem'
   }}
-  type="password" name="password" onChange={handleChange} />  
-   </form> 
+  autoComplete="current-password"
+  type="password" name="password" value={password} onChange={handleChange} /> 
+   
+   </Box>
+
 <Button variant="outlined" size="large" sx={{mt:4}}
  type="submit">Log In</Button>
 
